4856b9a0caf5199f6f0e22b9668674d6d7bb4720
Kit Foster <katya.a.foster@gmail.com>
2025-10-27 17:29:13 +0100

    only strip if the cookie is set

--- packages/microfrontends/src/bin/local-proxy.ts
+++ packages/microfrontends/src/bin/local-proxy.ts
@@ -609,7 +609,7 @@ export class LocalProxy {
         headers: {
           ...req.headers,
           host: hostname,
-          cookie: req.headers.cookie
+          cookie: req.headers.cookie?.includes('VERCEL_MFE_DEBUG')
             ? `${req.headers.cookie};VERCEL_MFE_DEBUG=;` // strip VERCEL_MFE_DEBUG cookie if present, as this causes an auth redirect
             : req.headers.cookie,
           ...(automationBypass
