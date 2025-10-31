8c370b6a81c0564f5c8bd46f22f46bd3cdcbae3e
Kit Foster <katya.a.foster@gmail.com>
2025-10-31 14:55:08 +0100

    Route to local proxy when turbo proxy is running

--- packages/microfrontends/src/next/utils/route-to-local-proxy.ts
+++ packages/microfrontends/src/next/utils/route-to-local-proxy.ts
@@ -1,5 +1,3 @@
 export function routeToLocalProxy(): boolean {
-  const isDevEnv = (process.env.VERCEL_ENV ?? 'development') === 'development';
-
-  return isDevEnv && Boolean(process.env.TURBO_TASK_HAS_MFE_PROXY);
+  return Boolean(process.env.TURBO_TASK_HAS_MFE_PROXY);
 }
